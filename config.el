5caeb72ef84120fdc7c211471984f06eaa3b28e6
milan144 <milan.hommet@protonmail.com>
2024-03-26 21:44:15 +0100

    changing theme

--- config.el
+++ config.el
@@ -1,6 +1,5 @@
 (setq doom-font (font-spec :family "JetbrainsMono Nerd Font" :size 15))
-;;(setq doom-theme 'doom-one)
-(setq doom-theme 'catppuccin)
+(setq doom-theme 'doom-one)
 (setq display-line-numbers-type t)
 (setq org-directory "~/Documents/")
 (add-to-list 'default-frame-alist '(alpha . 90))
--- custom.el
+++ custom.el
@@ -3,12 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("d77d6ba33442dd3121b44e20af28f1fae8eeda413b2c3d3b9f1315fbda021992" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages '(wakatime-mode typescript-mode catppuccin-theme))
- '(wakatime-api-key (getenv "WAKATIME_API_KEY"))
- '(wakatime-cli-path "~/.wakatime/wakatime-cli"))
+ '(package-selected-packages '(wakatime-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
