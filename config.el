47c1b43bc8a086b130bbf46dc116f13febe81a61
Milan Hommet <mhommet@teicee.com>
2024-03-27 12:58:20 +0100

    refactor(config.el): better organization

--- config.el
+++ config.el
@@ -1,12 +1,25 @@
+;; Font
 (setq doom-font (font-spec :family "JetbrainsMono Nerd Font" :size 15))
+
+;; Theme
 (setq doom-theme 'doom-one)
+
+;; Line numbers
 (setq display-line-numbers-type t)
-(setq org-directory "~/Documents/")
+
+;; Window
 (add-to-list 'default-frame-alist '(alpha . 90))
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
+;; Indentation
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
+
+;; Org mode
+(setq org-directory "~/Documents/")
+
+;; Wakatime
 (use-package wakatime-mode
   :ensure t)
 
