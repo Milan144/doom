29fb3dd2ad2e641f7129983b8e0841ad70f52b9e
Milan Hommet <mhommet@teicee.com>
2024-04-30 17:18:19 +0200

    d

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(doom-modeline-now-playing wakatime-mode typescript-mode php-cs-fixer catppuccin-theme)))
+   '(wakatime-mode typescript-mode php-cs-fixer catppuccin-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
