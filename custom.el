ee421f52a630d41818adbff80f82291627ff5d69
Milan Hommet <mhommet@teicee.com>
2024-04-30 17:17:06 +0200

    d

--- custom.el
+++ custom.el
@@ -2,8 +2,16 @@
 (setq wakatime-cli-path "~/.wakatime/wakatime-cli")
 
 (custom-set-variables
- ;; other custom variables go here
- )
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(doom-modeline-now-playing wakatime-mode typescript-mode php-cs-fixer catppuccin-theme)))
 (custom-set-faces
- ;; custom faces go here
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
