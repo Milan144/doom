7d5f4460bc459143e037cd00e2cac82c697695c5
Milan144 <milan.hommet@protonmail.com>
2024-09-05 14:43:00 +0200

    config for writing

--- config.el
+++ config.el
@@ -3,7 +3,7 @@
       doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16))
 
 ;; Transparency
-(add-to-list 'default-frame-alist '(alpha . 95))
+(add-to-list 'default-frame-alist '(alpha-background . 90))
 
 ;; Prevents some cases of Emacs flickering.
 (add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
@@ -15,14 +15,17 @@
 ;; An evil mode indicator is redundant with cursor shape
 (setq doom-modeline-modal nil)
 
-;;; :ui doom-dashboard
-(setq fancy-splash-image (file-name-concat doom-user-dir "splash.png"))
 ;; Hide the menu for as minimalistic a startup screen as possible.
 (setq +doom-dashboard-functions '(doom-dashboard-widget-banner))
 
 ;; Focus new window after splitting
 (setq evil-split-window-below t
       evil-vsplit-window-right t)
+
+;; Set space e to open treemacs and close it
+(map! :leader
+      :desc "Toggle treemacs"
+      "e" #'treemacs)
 
 ;; Org mode
 (setq org-modern-label-border nil)
@@ -37,12 +40,3 @@
   (font-lock-add-keywords 'org-mode
                           '(("^ *\\([-]\\) "
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
-
-;; accept completion from copilot and fallback to company
-(use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
-              ("C-<tab>" . 'copilot-accept-completion-by-word)))
--- packages.el
+++ packages.el
@@ -3,6 +3,4 @@
 
 ;; CUSTOM PACKAGES
 (package! pdf-tools)
-(package! copilot
-  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
 (package! org-modern)
